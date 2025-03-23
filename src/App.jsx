ece8b2e6b2f9f796efd19dcb80dddacf2e3f867a
AbdulRafeyMubasshir <armubbu@gmail.com>
2025-03-23 17:10:21 +0000

    API from render to EC2

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
       setInput('');
 
       try {
-        const response = await axios.post('https://renderflaskpr.onrender.com/predict', {
+        const response = await axios.post('http://13.53.174.248/predict', {
           text: input,
         });
 
